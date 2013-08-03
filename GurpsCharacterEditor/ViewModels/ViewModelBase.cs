a69aa56a1876004d4c06a062a5c5436436a17df9
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2013-08-04 01:00:44 +0200

    Support property dependencies

--- GurpsCharacterEditor/ViewModels/ViewModelBase.cs
+++ GurpsCharacterEditor/ViewModels/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace GurpsCharacterEditor.ViewModels
 {
@@ -7,11 +8,26 @@ namespace GurpsCharacterEditor.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
+        // Dictionary which holds information about which property is dependent on which. This is used by the
+        // property change notification system.
+        //
+        // Key: a property of the viewmodel.
+        // Value: an array of properties, which are dependent on the property.
+        protected Dictionary<string, string[]> PropertyDependencyMap = new Dictionary<string, string[]>();
+
         // Notify registered listeners that a change has happened. Used when setting property values.
         protected void NotifyPropertyChanged(string property)
         {
             if (PropertyChanged != null)
+            {
+                // Notify property change
                 PropertyChanged(this, new PropertyChangedEventArgs(property));
+
+                // Notify dependencies, if any
+                if (PropertyDependencyMap.ContainsKey(property))
+                    foreach (string dependencyProperty in PropertyDependencyMap[property])
+                        NotifyPropertyChanged(dependencyProperty);
+            }
         }
     }
 }
