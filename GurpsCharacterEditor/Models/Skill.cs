e907b2f9d9f44eae5b29f63c3b0708c0b576bd2a
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2013-12-14 18:37:06 +0100

    Add Skill class

---
+++ GurpsCharacterEditor/Models/Skill.cs
@@ -0,0 +1,104 @@
+﻿namespace GurpsCharacterEditor.Models
+{
+    // This enum is used to denote the difficulty of a skill.
+    public enum SkillDifficulty { Easy, Average, Hard, VeryHard };
+
+    // This enum is used to specify which stat a skill is based on.
+    public enum SkillStat { Strength, Dexterity, Intelligence, Health }
+
+    public class Skill
+    {
+        Character character;
+
+        // Name of the skill
+        public string Name { get; set; }
+
+        // The stat this skill is based on.
+        public SkillStat Stat { get; set; }
+
+        // The difficulty of the skill
+        public SkillDifficulty Difficulty { get; set; }
+
+        // The skill level relative to the base skill level.
+        public int? RelativeLevel { get; set; }
+
+        // The number of character points the training of this level is worth.
+        public int Points
+        {
+            get
+            {
+                // If not learned, then 0 points are spent.
+                if (RelativeLevel == null)
+                    return 0;
+
+                // Compensate for skill difficulty.
+                int level = (int)RelativeLevel;
+                switch (Difficulty)
+                {
+                    case SkillDifficulty.Average:
+                        level += 1;
+                        break;
+                    case SkillDifficulty.Hard:
+                        level += 2;
+                        break;
+                    case SkillDifficulty.VeryHard:
+                        level += 3;
+                        break;
+                }
+
+                // Calculate number of points spent if difficulty is easy.
+                int points = 0;
+                if (level == 0)
+                    points = 1;
+                if (level == 1)
+                    points = 2;
+                if (level > 1)
+                    points = 4 * (level - 1);
+
+                return points;
+            }
+        }
+
+        // The effective skill level.
+        public int? Level
+        {
+            get
+            {
+                // If skill is not learned, skill level is not applicable.
+                if (RelativeLevel == null)
+                    return null;
+                else
+                {
+                    // Add base skill level to relative level.
+                    switch (Stat)
+                    {
+                        case SkillStat.Strength:
+                            return character.Strength + (int)RelativeLevel;
+                        case SkillStat.Dexterity:
+                            return character.Dexterity + (int)RelativeLevel;
+                        case SkillStat.Intelligence:
+                            return character.Intelligence + (int)RelativeLevel;
+                        case SkillStat.Health:
+                            return character.Health + (int)RelativeLevel;
+                        default:
+                            return null;
+                    }
+                }
+            }
+        }
+
+        public Skill(Character character, string name, SkillStat stat, SkillDifficulty difficulty)
+        {
+            this.character = character;
+            Name = name;
+            Stat = stat;
+            Difficulty = difficulty;
+            RelativeLevel = null;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ GurpsCharacterEditorTests/Models/SkillTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GurpsCharacterEditor.Models;
+
+namespace GurpsEditorTests.Models
+{
+    [TestClass]
+    public class SkillTest
+    {
+        [TestMethod]
+        public void SkillPoints()
+        {
+            Character character = new Character();
+            character.StrengthPoints = 1;
+            character.DexterityPoints = 2;
+            character.IntelligencePoints = 3;
+            character.HealthPoints = 4;
+
+            Skill target;
+
+            target = new Skill(character, "abc", SkillStat.Strength, SkillDifficulty.Average);
+            Assert.AreEqual(0, target.Points);
+
+            target = new Skill(character, "abc", SkillStat.Intelligence, SkillDifficulty.Easy);
+            target.RelativeLevel = 2;
+            Assert.AreEqual(4, target.Points);
+
+            target = new Skill(character, "abc", SkillStat.Dexterity, SkillDifficulty.Average);
+            target.RelativeLevel = 0;
+            Assert.AreEqual(2, target.Points);
+
+            target = new Skill(character, "abc", SkillStat.Health, SkillDifficulty.VeryHard);
+            target.RelativeLevel = -1;
+            Assert.AreEqual(4, target.Points);
+
+            target = new Skill(character, "abc", SkillStat.Strength, SkillDifficulty.Hard);
+            target.RelativeLevel = 3;
+            Assert.AreEqual(16, target.Points);
+        }
+
+        [TestMethod]
+        public void SkillLevel()
+        {
+            Character character = new Character();
+            character.StrengthPoints = 1;
+            character.DexterityPoints = 2;
+            character.IntelligencePoints = 3;
+            character.HealthPoints = 4;
+
+            Skill target;
+
+            target = new Skill(character, "abc", SkillStat.Intelligence, SkillDifficulty.Easy);
+            Assert.IsNull(target.Level);
+            target.RelativeLevel = 2;
+            Assert.AreEqual(15, target.Level);
+
+            target = new Skill(character, "abc", SkillStat.Dexterity, SkillDifficulty.Average);
+            Assert.IsNull(target.Level);
+            target.RelativeLevel = 0;
+            Assert.AreEqual(12, target.Level);
+
+            target = new Skill(character, "abc", SkillStat.Health, SkillDifficulty.VeryHard);
+            Assert.IsNull(target.Level);
+            target.RelativeLevel = -1;
+            Assert.AreEqual(13, target.Level);
+
+            target = new Skill(character, "abc", SkillStat.Strength, SkillDifficulty.Hard);
+            Assert.IsNull(target.Level);
+            target.RelativeLevel = 3;
+            Assert.AreEqual(14, target.Level);
+        }
+    }
+}
