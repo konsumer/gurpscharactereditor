0996254dd4b30f0e08022165bdd512ae967c0d3d
Claus Jensby Madsen <clausjensbymadsen@gmail.com>
2014-03-21 23:57:19 +0100

    Add Damage and Weapon class

---
+++ GurpsCharacterEditor/Models/Damage.cs
@@ -0,0 +1,17 @@
+﻿namespace GurpsCharacterEditor.Models
+{
+    public enum DamageBase { Swinging, Thrusting }
+    public enum DamageType { Affliction, Burning, Corrosion, Crushing, Cutting, Fatigue, Impaling, SmallPiercing, Piercing, LargePiercing, HugePiercing, Toxic, Special };
+
+    public class Damage
+    {
+        public DamageBase DamageBase { get; set; }
+        public DamageType DamageType { get; set; }
+
+        public Damage(DamageBase damageBase, DamageType damageType)
+        {
+            DamageBase = damageBase;
+            DamageType = damageType;
+        }
+    }
+}
---
+++ GurpsCharacterEditor/Models/Weapon.cs
@@ -0,0 +1,18 @@
+﻿namespace GurpsCharacterEditor.Models
+{
+    public class Weapon : Item
+    {
+        public int MinimumStrength { get; set; }
+
+        public Weapon()
+            : base()
+        {
+        }
+
+        public Weapon(string name, int value, int weight)
+            : base(name, value, weight)
+        {
+            MinimumStrength = 0;
+        }
+    }
+}
